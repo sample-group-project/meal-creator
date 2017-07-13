9e89c79b8be02b630f2cf38c857b1e5ad6722aa4
Bruin324 <bruin324@gmAIL.COM>
2017-07-13 11:15:08 -0400

    getting robs model

--- server.js
+++ server.js
@@ -15,7 +15,22 @@ application.use(bodyParser.urlencoded());
 application.use('/public', express.static('./public'));
 
 application.get('/', (request, response) => {
+    response.redirect('/index');
+});
+
+application.get('/index', (request, response) => {
     response.render('index');
 });
+
+application.post('/index/add-meal', (request, response) => {
+    var mealType = request.body.type;
+    var mealName = request.body.name;
+
+    
+});
+// model = {
+//     description: meal.description,
+//     calories: meal.calories
+// }
 
 application.listen(3000);
